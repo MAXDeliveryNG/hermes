355a260db25148143fa9d17070b93590ff27b867
Bond <bond@max.ng>
2018-10-05 17:07:57 +0100

    format codebase

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { ServiceBus } from './servicebus';
 import { ClientOptions } from '@azure/event-hubs';
 
 export namespace Hermes {
-    export function createEventHub(connectionString: string, hubName: string, options?: ClientOptions): EventHub {
-        return new EventHub(connectionString, hubName, options);
-    }
+  export function createEventHub(
+    connectionString: string,
+    hubName: string,
+    options?: ClientOptions
+  ): EventHub {
+    return new EventHub(connectionString, hubName, options);
+  }
 
-    export function createServiceBus(connectionString: string,): ServiceBus {
-        return new ServiceBus(connectionString);
-    }
+  export function createServiceBus(connectionString: string): ServiceBus {
+    return new ServiceBus(connectionString);
+  }
 }
